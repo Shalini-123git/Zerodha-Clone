4114b29494dce850cd77569bd096dc9121227a88
shalini rajput <rajputshalini8744@gmail.com>
2026-04-28 21:52:48 +0530

    Added Support Component

--- frontend/src/landing_page/supports/SupportPage.jsx
+++ frontend/src/landing_page/supports/SupportPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import Hero from "./Hero";
+import CreateTicket from "./CreateTicket";
+
 function SupportPage() {
-    return (
-        <div>
-            <h1>support page</h1>
-        </div>
-    )
+  return (
+    <>
+      <Hero />
+      <CreateTicket />
+    </>
+  );
 }
 
 export default SupportPage;
