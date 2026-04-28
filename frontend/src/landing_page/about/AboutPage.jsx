6512c05b7deef0f74769e4ce7e2b3580264d8887
shalini rajput <rajputshalini8744@gmail.com>
2026-04-28 21:21:45 +0530

    Add AboutPage Component

--- frontend/src/landing_page/about/AboutPage.jsx
+++ frontend/src/landing_page/about/AboutPage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+
 import Hero from "./Hero";
-import Team from './Hero';
+import Team from "./Team";
 
 function AboutPage() {
-    return (
-        <>
-            <Hero/>
-            <Team/>
-        </>
-    )
+  return (
+    <>
+      <Hero />
+      <Team />
+    </>
+  );
 }
 
 export default AboutPage;
