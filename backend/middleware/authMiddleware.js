d38899bc9f6cf79b784e136e1a9a26faaf284f20
shalini rajput <rajputshalini8744@gmail.com>
2026-05-19 16:32:06 +0530

    Return user from auth verification

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -18,7 +18,7 @@ export const userVerification = (req, res) => {
             })
         }else{
             const user = await User.findById(data.id);
-            if(user) return res.json({ status: true })
+            if(user) return res.json({ status: true, user })
             else return res.json({ status: false });
         }
     })
