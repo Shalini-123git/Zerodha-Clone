3c754fe8d642fb00939b6fd34175fafd6238121a
shalini rajput <rajputshalini8744@gmail.com>
2026-05-19 16:32:05 +0530

    Load dashboard user session

--- Dashboard/src/components/Home.jsx
+++ Dashboard/src/components/Home.jsx
@@ -1,13 +1,46 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import Dashboard from "./Dashboard.jsx";
 import TopBar from "./TopBar.jsx";
 
 const Home = () => {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        console.log("api url", `${import.meta.env.VITE_API_URL}/auth/me`)
+        const res = await axios.get(
+          `${import.meta.env.VITE_API_URL}/auth/me`,
+          {
+            withCredentials: true,
+          }
+        );
+
+        if (!res.data.success) {
+          window.location.href = import.meta.env.VITE_FRONTEND_URL + "/login";
+          return;
+        }
+
+        setUser(res.data.user);
+
+      } catch (error) {
+        console.log(import.meta.env.VITE_FRONTEND_URL + "/login")
+        window.location.href = import.meta.env.VITE_FRONTEND_URL + "/login";
+      }
+    };
+
+    getUser();
+  }, []);
+
+  if (!user) return <div>Loading...</div>;
+
+
   return (
     <>
-      <TopBar />
-      <Dashboard />
+      <TopBar user={user}/>
+      <Dashboard user={user} />
     </>
   );
 };
